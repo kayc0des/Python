235eaa17aaf5216ddabe0cb12a2cb20bb1c2d245
kayc0des <k.boafo@alustudent.com>
2022-11-02 10:39:39 +0200

    Variables and Data Types

---
+++ pythonBasics.py
@@ -0,0 +1,15 @@
+#The print statement prints to stdout
+#Comments in Python use the hash symbol
+
+name = input("What is your name? ") #accept input from user
+print("Nice to meet you", name)
+
+print("Welcome to ChatBot")
+
+
+sal = 10000.23 
+print(type(sal))
+
+#AlU print integer
+number = 98
+print(f"{number} Battery Street")
