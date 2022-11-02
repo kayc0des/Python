adb90ce1d38174b8c3757f4db1987ab3b9c2d1e8
kayc0des <k.boafo@alustudent.com>
2022-11-02 10:48:33 +0200

    String commit

--- pythonBasics.py
+++ pythonBasics.py
@@ -1,10 +1,10 @@
 #The print statement prints to stdout
 #Comments in Python use the hash symbol
 
-name = input("What is your name? ") #accept input from user
-print("Nice to meet you", name)
+#name = input("What is your name? ") #accept input from user
+#print("Nice to meet you", name)
 
-print("Welcome to ChatBot")
+#print("Welcome to ChatBot")
 
 
 sal = 10000.23 
@@ -13,3 +13,10 @@ print(type(sal))
 #AlU print integer
 number = 98
 print(f"{number} Battery Street")
+
+number = 3.14159
+print(f"Float: {number:.2f}")
+
+str = "Holberton School"
+print(str*3)
+print(str[:9])
