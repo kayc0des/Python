bf0d933a1256fe39bfae79a73c0e9a8a9186e88e
kayc0des <k.boafo@alustudent.com>
2022-11-03 11:43:35 +0200

    Bubble Sort Algorithm

--- derekList.py
+++ derekList.py
@@ -8,6 +8,39 @@ for i in range(5):
     randList.append(n)
     #randList.append(random.randrange(1,9))
 print(randList)
-for i in randList:
-    print(i)
+#for i in randList:
+#   print(i)
 
+print("Bubble Sort Algorithm")
+
+i = len(randList) - 1
+
+print(i)
+
+while i > 1:
+    j = 0
+
+    while j < i:
+
+        print("\nIs {} > {}".format(randList[j], randList[j+1]))
+        
+        if randList[j] > randList[j + 1]:
+
+            print("Switch")
+            temp = randList[j]
+            randList[j] = randList[j + 1]
+            randList[j + 1] = temp
+        else:
+            print("Dont't Switch")
+        j += 1
+
+        for k in randList:
+            print(k, end=", ")
+        print()
+
+    print("END OF ROUND") 
+    
+    i -= 1
+for k in randList:
+    print(k, end=", ")
+print() 
